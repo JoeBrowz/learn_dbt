cfc56ae0fb98ad6e05c1f2e22a47f0f8dbf520e9
Joe Marx <josephlmarx@gmail.com>
2021-06-11 16:49:00 +0000

    modularize model

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -3,43 +3,47 @@
 )}}
 
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    
-    from raw.jaffle_shop.customers
+
+    select * from {{ ref('stg_customers')}}
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_orders') }}
+
 ),
 
 customer_orders as (
 
-  select
-    user_id as customer_id,
-        
+    select
+        customer_id,
+
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(id) as number_of_orders
-  
-  from raw.Jaffle_shop.orders
+        count(order_id) as number_of_orders
 
-  group by 1
+    from orders
+
+    group by 1
 
 ),
 
+
 final as (
-  
-  select
-    customers.customer_id,
-    customers.first_name,
-    customers.last_name,
-    customer_orders.first_order_date,
-    customer_orders.most_recent_order_date,
-    coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-  
-  from customers
-  
-  left join customer_orders using (customer_id)
-  
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+    from customers
+
+    left join customer_orders using (customer_id)
+
 )
 
 select * from final
