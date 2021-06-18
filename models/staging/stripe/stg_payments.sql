11b6050e4327a3675069f098aa533473f6a4b299
Joe Marx <josephlmarx@gmail.com>
2021-06-18 02:37:14 +0000

    added assertion test

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,15 +1,11 @@
-with payment as (
+select
+    id as payment_id,
+    orderid as order_id,
+    
+    -- convert amount in cents to dollars
+    amount / 100 as amount,
+    created as sale_date,
+    paymentmethod as payment_method,
+    status
 
-    select
-        id as payment_id,
-        orderid as order_id,
-        amount / 100 as amount,
-        created as sale_date,
-        paymentmethod as payment_method
-
-    from raw.stripe.payment
-    where status != 'fail'
-
-)
-
-select * from payment
+from raw.stripe.payment
